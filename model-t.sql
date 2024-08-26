33ee76bf96275b059801984c4833a165b02b91a3
OneWay033 <heromaketplay@gmail.com>
2024-08-27 00:44:44 +0700

    up-data

---
+++ model-t.sql
@@ -0,0 +1,457 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 26, 2024 at 07:43 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `model-t`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `admin_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE `clients` (
+  `client_id` int(11) NOT NULL,
+  `trainer_id` int(11) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `address` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`client_id`, `trainer_id`, `first_name`, `last_name`, `phone`, `address`) VALUES
+(5, 1, 'John', 'Doe', '+1-555-1234', '123 Elm Street, Springfield, IL'),
+(6, 1, 'Jane', 'Smith', '+1-555-5678', '456 Oak Avenue, Springfield, IL'),
+(7, 1, 'John', 'Doely', '0634567434', '164 Elm Street, Springfield, IL'),
+(8, 1, 'Jo', 'Doely', '063454544', '133 Elm Street, Springfield, IL'),
+(9, 1, 'male', 'kaka', '0876734321', 'Bkk 1234'),
+(10, 1, 'male', 'kaka', '0876734321', 'Bkk 1234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client_programs`
+--
+
+CREATE TABLE `client_programs` (
+  `client_program_id` int(11) NOT NULL,
+  `client_id` int(11) DEFAULT NULL,
+  `program_id` int(11) DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exercises`
+--
+
+CREATE TABLE `exercises` (
+  `exercise_id` int(11) NOT NULL,
+  `type_id` int(11) DEFAULT NULL,
+  `exercise_name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exercise_records`
+--
+
+CREATE TABLE `exercise_records` (
+  `record_id` int(11) NOT NULL,
+  `client_program_id` int(11) DEFAULT NULL,
+  `exercise_date` date DEFAULT NULL,
+  `exercise_done` tinyint(1) DEFAULT NULL,
+  `notes` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `goals`
+--
+
+CREATE TABLE `goals` (
+  `goal_id` int(11) NOT NULL,
+  `client_id` int(11) DEFAULT NULL,
+  `goal_type` varchar(50) DEFAULT NULL,
+  `bmr` float DEFAULT NULL,
+  `tdee` float DEFAULT NULL,
+  `bmi` float DEFAULT NULL,
+  `weight` float DEFAULT NULL,
+  `height` float DEFAULT NULL,
+  `waist` float DEFAULT NULL,
+  `chest` float DEFAULT NULL,
+  `arm` float DEFAULT NULL,
+  `leg` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trainers`
+--
+
+CREATE TABLE `trainers` (
+  `trainer_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `address` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `trainers`
+--
+
+INSERT INTO `trainers` (`trainer_id`, `user_id`, `first_name`, `last_name`, `phone`, `address`) VALUES
+(1, 2, 'Nine', 'Hensom', '0638573894', 'BBK ');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `training_programs`
+--
+
+CREATE TABLE `training_programs` (
+  `program_id` int(11) NOT NULL,
+  `program_name` varchar(100) DEFAULT NULL,
+  `exercise_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `types`
+--
+
+CREATE TABLE `types` (
+  `type_id` int(11) NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `type_name` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `update_clients`
+--
+
+CREATE TABLE `update_clients` (
+  `update_id` int(11) NOT NULL,
+  `client_id` int(11) DEFAULT NULL,
+  `update_date` date DEFAULT NULL,
+  `weight` float DEFAULT NULL,
+  `height` float DEFAULT NULL,
+  `bmr` float DEFAULT NULL,
+  `tdee` float DEFAULT NULL,
+  `bmi` float DEFAULT NULL,
+  `waist` float DEFAULT NULL,
+  `chest` float DEFAULT NULL,
+  `arm` float DEFAULT NULL,
+  `leg` float DEFAULT NULL,
+  `goal_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `role` enum('trainer','admin') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `role`) VALUES
+(1, 'admin', '1234', 'admin'),
+(2, 'user_01 ', '1234', 'trainer');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`admin_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`client_id`),
+  ADD KEY `trainer_id` (`trainer_id`);
+
+--
+-- Indexes for table `client_programs`
+--
+ALTER TABLE `client_programs`
+  ADD PRIMARY KEY (`client_program_id`),
+  ADD KEY `client_id` (`client_id`),
+  ADD KEY `program_id` (`program_id`);
+
+--
+-- Indexes for table `exercises`
+--
+ALTER TABLE `exercises`
+  ADD PRIMARY KEY (`exercise_id`),
+  ADD KEY `type_id` (`type_id`);
+
+--
+-- Indexes for table `exercise_records`
+--
+ALTER TABLE `exercise_records`
+  ADD PRIMARY KEY (`record_id`),
+  ADD KEY `client_program_id` (`client_program_id`);
+
+--
+-- Indexes for table `goals`
+--
+ALTER TABLE `goals`
+  ADD PRIMARY KEY (`goal_id`),
+  ADD KEY `client_id` (`client_id`);
+
+--
+-- Indexes for table `trainers`
+--
+ALTER TABLE `trainers`
+  ADD PRIMARY KEY (`trainer_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `training_programs`
+--
+ALTER TABLE `training_programs`
+  ADD PRIMARY KEY (`program_id`),
+  ADD KEY `fk_exercise` (`exercise_id`);
+
+--
+-- Indexes for table `types`
+--
+ALTER TABLE `types`
+  ADD PRIMARY KEY (`type_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `update_clients`
+--
+ALTER TABLE `update_clients`
+  ADD PRIMARY KEY (`update_id`),
+  ADD KEY `client_id` (`client_id`),
+  ADD KEY `fk_goal_id` (`goal_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `client_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `client_programs`
+--
+ALTER TABLE `client_programs`
+  MODIFY `client_program_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `exercises`
+--
+ALTER TABLE `exercises`
+  MODIFY `exercise_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `exercise_records`
+--
+ALTER TABLE `exercise_records`
+  MODIFY `record_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `goals`
+--
+ALTER TABLE `goals`
+  MODIFY `goal_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `trainers`
+--
+ALTER TABLE `trainers`
+  MODIFY `trainer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `training_programs`
+--
+ALTER TABLE `training_programs`
+  MODIFY `program_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `types`
+--
+ALTER TABLE `types`
+  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `update_clients`
+--
+ALTER TABLE `update_clients`
+  MODIFY `update_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `admins`
+--
+ALTER TABLE `admins`
+  ADD CONSTRAINT `admins_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `clients`
+--
+ALTER TABLE `clients`
+  ADD CONSTRAINT `clients_ibfk_1` FOREIGN KEY (`trainer_id`) REFERENCES `trainers` (`trainer_id`);
+
+--
+-- Constraints for table `client_programs`
+--
+ALTER TABLE `client_programs`
+  ADD CONSTRAINT `client_programs_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`),
+  ADD CONSTRAINT `client_programs_ibfk_2` FOREIGN KEY (`program_id`) REFERENCES `training_programs` (`program_id`);
+
+--
+-- Constraints for table `exercises`
+--
+ALTER TABLE `exercises`
+  ADD CONSTRAINT `exercises_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `types` (`type_id`);
+
+--
+-- Constraints for table `exercise_records`
+--
+ALTER TABLE `exercise_records`
+  ADD CONSTRAINT `exercise_records_ibfk_1` FOREIGN KEY (`client_program_id`) REFERENCES `client_programs` (`client_program_id`);
+
+--
+-- Constraints for table `goals`
+--
+ALTER TABLE `goals`
+  ADD CONSTRAINT `goals_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`);
+
+--
+-- Constraints for table `trainers`
+--
+ALTER TABLE `trainers`
+  ADD CONSTRAINT `trainers_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `training_programs`
+--
+ALTER TABLE `training_programs`
+  ADD CONSTRAINT `fk_exercise` FOREIGN KEY (`exercise_id`) REFERENCES `exercises` (`exercise_id`);
+
+--
+-- Constraints for table `types`
+--
+ALTER TABLE `types`
+  ADD CONSTRAINT `types_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`);
+
+--
+-- Constraints for table `update_clients`
+--
+ALTER TABLE `update_clients`
+  ADD CONSTRAINT `fk_goal_id` FOREIGN KEY (`goal_id`) REFERENCES `goals` (`goal_id`),
+  ADD CONSTRAINT `update_clients_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
